bbc30537d3a812058fffd7f1481719381697082f
Kildare Lauser <kildarealauser@gmail.com>
2019-08-26 15:58:45 -0400

    created project and add HomePage component

---
+++ src/components/HomePage/home-page.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./home-page.scss";
+
+export const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">HATS</h2>
+          <span className="subtititle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">HATS</h2>
+          <span className="subtititle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">HATS</h2>
+          <span className="subtititle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">HATS</h2>
+          <span className="subtititle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h2 className="title">HATS</h2>
+          <span className="subtititle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
