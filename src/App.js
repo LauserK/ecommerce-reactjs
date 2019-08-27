a588e381e69c883eb0463970fd817c388e247993
Kildare Lauser <kildarealauser@gmail.com>
2019-08-27 12:42:53 -0400

    add shope page, and collections components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
-
 import { HomePage } from "./pages/HomePage/home-page";
-
+import ShopPage from "./pages/ShopPage/shop-page";
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.scss";
-export const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+export const MenuItem = withRouter(
+  ({ title, imageUrl, size, history, linkUrl, match }) => (
     <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    ></div>
-    <div className="content">
-      <h2 className="title">{title.toUpperCase()}</h2>
-      <span className="subtitle">SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      ></div>
+      <div className="content">
+        <h2 className="title">{title.toUpperCase()}</h2>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
+  )
 );
